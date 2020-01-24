f660c6f883f5718bed0b595de08934ee81a5759e
AlexAlv <55999674+Alexander-Alvarado@users.noreply.github.com>
2020-01-23 20:42:18 -0500

    Updates

---
+++ precache-manifest.480dd953f1abbb6d0164dab509d07c5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a629c7b672875262b20a588be6b2421",
+    "url": "/tic-tack-toe/index.html"
+  },
+  {
+    "revision": "f5b153142207acec28a9",
+    "url": "/tic-tack-toe/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "13b23b1d00a85aa832aa",
+    "url": "/tic-tack-toe/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f5b153142207acec28a9",
+    "url": "/tic-tack-toe/static/js/2.3db26b0b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tack-toe/static/js/2.3db26b0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13b23b1d00a85aa832aa",
+    "url": "/tic-tack-toe/static/js/main.7a7bf517.chunk.js"
+  },
+  {
+    "revision": "41b5ed81f44ec69084f2",
+    "url": "/tic-tack-toe/static/js/runtime-main.cfbdd667.js"
+  }
+]);
